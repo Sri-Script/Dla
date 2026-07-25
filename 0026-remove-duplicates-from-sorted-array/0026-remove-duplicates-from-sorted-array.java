387b132baa062ad9b9a794289943f020ed1afdfa
Sritama Chatterjee <hiresritamach@gmail.com>
2026-07-25 16:50:05 +0530

    Update 0026-remove-duplicates-from-sorted-array.java

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int write = 1;
+        int write = 1;//The first element is automatically unique because nothing comes before it
 
         for (int read = 1; read < nums.length; read++) {
             if (nums[read] != nums[write - 1]) {
